398514a8b4744e8d48fe71ddf654d3cbdcadccac
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-29 11:26:30 +0300

    woo jauns progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
  import Diena from "./Diena.js";
  function App(){
+    const pirmdienasStundas = ["sports", "dabaszinibas", "vesture", "krievu valoda"];
+    const otrdienasStundas = ["sports2", "dabaszinibas2", "vesture2", "krievu valoda2"];
     return (
         <>
-    <div>Saja stundu saraksta ir tikai programesana pie JEKABA</div>
-    <img src="https://scontent.frix3-1.fna.fbcdn.net/v/t1.6435-9/122008302_3581071571916079_8757671365586823827_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=xopwI5CsSYwAX8HYwHo&_nc_ht=scontent.frix3-1.fna&oh=00_AfADClcI464DwwDPgRiF8LSXeqp1Jn4rt1MHd6634svlNw&oe=653CCCEC" alt="React Image"></img>;
-    <Diena/>
+    
+    <Diena diena="Pirmdiena" stundas={pirmdienasStundas}/>
+    <Diena diena="Otrdiena" stundas={otrdienasStundas}/>
 </>
 )
 }
--- src/Diena.js
+++ src/Diena.js
@@ -1,4 +1,15 @@
-function Diena() {
-    return <p>Hi diena</p>
+import Stunda from "./Stunda.js";
+function Diena(props) {
+    return (
+    <>
+    <h2>{props.diena}</h2>
+    <ul>
+        <Stunda nosaukums= {props.stundas[0]}/>
+        <Stunda nosaukums={props.stundas[1]}/>
+        <Stunda nosaukums={props.stundas[2]}/>
+        <Stunda nosaukums={props.stundas[3]}/>
+    </ul>
+    </>
+    )
 };
 export default Diena;
